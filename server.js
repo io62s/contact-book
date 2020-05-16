66a2086c3218ff2e1acedb0c4a964fc366d16c76
io62s <ufo62s@gmail.com>
2020-05-16 20:05:32 +0200

    connection to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect to mongoDB
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ msg: "Contact Book Home Page" });
