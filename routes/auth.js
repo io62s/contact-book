b65cea8ef178ae5689cde88be792b7d1b1922b8b
io62s <ufo62s@gmail.com>
2020-05-19 00:53:24 +0200

    login user

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
 //get loged in user - /api/auth
-router.get("/", (req, res) => {
-  res.send("Get Logged in User");
+router.get("/", auth, async (req, res) => {
+  const { id } = req.user; //comming from auth middleware
+  try {
+    const user = await User.findById(id).select("-password"); //don't return pass in res
+    res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 //Auth user, get token - /api/auth
