f55085788f3afb5b99d03fad58906ee05e89fb74
io62s <ufo62s@gmail.com>
2020-05-20 22:29:01 +0200

    contact model, get all contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 //get all users contacts - /api/contacts
-router.get("/", (req, res) => {
-  res.send("Users contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    const { id } = req.user;
+    const contacts = await Contact.find({ user: id }).sort({ date: -1 });
+
+    res.json(contacts);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 //add new contact - /api/contacts
 router.post("/", (req, res) => {
