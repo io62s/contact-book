89dbbf102bc8fc7394696c36bd81501b7aacdc06
io62s <ufo62s@gmail.com>
2020-05-30 22:35:24 +0200

    auth context blrplt

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,31 @@ import NavBar from "./components/layout/NavBar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 import "./App.css";
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/about">
-                <About />
-              </Route>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/about">
+                  <About />
+                </Route>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
