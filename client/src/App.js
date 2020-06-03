48d96623da5b83ae08ab075c0807949d9ec9359e
io62s <ufo62s@gmail.com>
2020-06-03 23:54:04 +0200

    alert context, reducer setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
@@ -14,27 +15,29 @@ function App() {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <NavBar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path="/about">
-                  <About />
-                </Route>
-                <Route exact path="/register">
-                  <Register />
-                </Route>
-                <Route exact path="/login">
-                  <Login />
-                </Route>
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <NavBar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/">
+                    <Home />
+                  </Route>
+                  <Route exact path="/about">
+                    <About />
+                  </Route>
+                  <Route exact path="/register">
+                    <Register />
+                  </Route>
+                  <Route exact path="/login">
+                    <Login />
+                  </Route>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
