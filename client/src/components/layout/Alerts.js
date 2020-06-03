05a9758d66383764260455234deac7b06fabb725
io62s <ufo62s@gmail.com>
2020-06-04 00:30:31 +0200

    alert msg transition animation added

--- client/src/components/layout/Alerts.js
+++ client/src/components/layout/Alerts.js
@@ -1,16 +1,22 @@
 import React, { useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import AlertContext from "../../context/alert/alertContext";
 
 function Alerts() {
   const alertContext = useContext(AlertContext);
+  const { alerts } = alertContext;
 
   return (
-    alertContext.alerts.length > 0 &&
-    alertContext.alerts.map((alert) => (
-      <div key={alert.id} className={`alert alert-${alert.type}`}>
-        <i className="fas fa-info-circle" /> {alert.msg}
-      </div>
-    ))
+    <TransitionGroup>
+      {alerts.length > 0 &&
+        alerts.map(({ id, type, msg }) => (
+          <CSSTransition key={id} timeout={300} classNames="item">
+            <div className={`alert alert-${type}`}>
+              <i className="fas fa-info-circle" /> {msg}
+            </div>
+          </CSSTransition>
+        ))}
+    </TransitionGroup>
   );
 }
 
