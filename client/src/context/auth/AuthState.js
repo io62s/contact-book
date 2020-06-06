eedef317e11d6eae0da8b382061376cf01b25c20
io62s <ufo62s@gmail.com>
2020-06-06 23:52:12 +0200

    register user

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from "react";
+import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
 
@@ -24,6 +25,30 @@ function AuthState(props) {
 
   const [state, dispatch] = useReducer(authReducer, initialState);
 
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/users", formData, config);
+      const userToken = await res.data;
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: userToken,
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
+
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+
   return (
     <AuthContext.Provider
       value={{
@@ -32,6 +57,8 @@ function AuthState(props) {
         loading: state.loading,
         user: state.user,
         error: state.error,
+        register,
+        clearErrors,
       }}
     >
       {props.children}
